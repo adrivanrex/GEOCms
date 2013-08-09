9631c2df92ccff81e47812b0527093350a45bc53
JackolanternIR <dacypher@gmail.com>
2013-08-09 05:16:39 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,7 @@
 /*
  * GET home page.
  */
- var io = require('socket.io').listen(8000);
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-  
  
-
-  io.sockets.on('connection', function (socket) {
-  socket.broadcast.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
-  
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });  
 };
